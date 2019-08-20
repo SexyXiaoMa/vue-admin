1a9d0b8c01e7d25055e4993dc5c8137aa980e7d2
maliang <maliang@vchangyi.com>
2019-08-20 16:51:46 +0800

    功能迭代

--- src/utils/register_router.js
+++ src/utils/register_router.js
@@ -6,28 +6,39 @@
 // 基础路由
 import Login from "@/views/base/login/login";
 import Default from "@/views/base/default/default";
+import Denied from "@/views/base/denied/denied";
 
 // 应用路由
 import router from "./collect_router";
 
 let routes = [
   {
+    // 登录路由
     path: "/login",
     name: "login",
     component: Login
   },
   {
-    path: "/404",
+    // 默认路由
+    path: "/default",
     name: "default",
     component: Default
   },
   {
+    // 权限不足路由
+    path: "/permission_denied",
+    name: "denied",
+    component: Denied
+  },
+  {
+    // 默认路由
     path: "/",
     redirect: {
       name: "login"
     }
   },
   {
+    // 路由不存在
     path: "*",
     redirect: {
       name: "default"
---
+++ src/views/base/denied/denied.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>I am denied</div>
+</template>
